61e610435998258232d9ac567c42caa436804300
VANSHITAWEBEARL <vanshita.webearl@gmail.com>
2026-01-31 19:18:37 +0530

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -115,7 +115,7 @@ export default function Footer() {
                 <svg className="w-5 h-5 text-secondary-400 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                <a href="tel:+919054372690" className="hover:text-secondary-400 transition-colors">+91 9054372690</a>
+                <a href="tel:+916355582605" className="hover:text-secondary-400 transition-colors">+91 6355582605</a>
               </li>
             </ul>
           </div>
